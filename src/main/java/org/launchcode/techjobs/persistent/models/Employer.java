01bf21a310b42901b66336e27bb24feac0224d99
Timothy Nicolas <timnicolas87@gmail.com>
2023-11-10 01:23:01 -0600

    GA4 - Task 2 - Entities and Repositories

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,4 +1,29 @@
 package org.launchcode.techjobs.persistent.models;
 
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+
+@Entity
 public class Employer extends AbstractEntity {
+
+    @NotBlank(message = "Please enter a location.")
+    @Size(max = 120, message = "Location must be under 120 characters.")
+    private String location;
+
+    public Employer(String location) {
+        this.location = location;
+    }
+
+    public Employer() {
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
